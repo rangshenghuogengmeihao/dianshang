f2658649c8d174ef193cb27a5df73c0d42869466
rangshenghuogengmeihao <1744982671@qq.com>
2022-04-10 10:58:45 +0800

    1.0.04

--- src/main/scala/Ti1/B_task1_1_CUSTOMER.scala
+++ src/main/scala/Ti1/B_task1_1_CUSTOMER.scala
@@ -2,15 +2,38 @@ package Ti1
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 object B_task1_1_CUSTOMER {
   def main(args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("B_task1_1_CUSTOMER")
-    val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+    val sparkSession = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate()
     import sparkSession.implicits._
-    sparkSession.read
+    val dataFrame = sparkSession.read
       .format("jdbc")
-      .option("driver","com.mysql.jdbc.Driver")
-//      .option("")
+      .option("driver", "com.mysql.jdbc.Driver")
+      .option("url", "jdbc:mysql://192.168.23.69:3306/shtd_store")
+      .option("user","root")
+      .option("password","123456")
+      .option("dbtable","CUSTOMER")
+      .load()
+
+    val structType = StructType(List(
+      StructField("CUSTKEY", StringType, nullable = true),
+      StructField("NAME", StringType, nullable = true),
+      StructField("ADDRESS", StringType, nullable = true),
+      StructField("NATIONKEY", StringType, nullable = true),
+      StructField("PHONE", StringType, nullable = true),
+      StructField("ACCTBAL", StringType, nullable = true),
+      StructField("MKTSEGMENT", StringType, nullable = true),
+      StructField("COMMENT", StringType, nullable = true)
+    ))
+
+
+//    dataFrame.show()
+    dataFrame.write
+//      .format()
+
+    sparkSession.close()
   }
 }
